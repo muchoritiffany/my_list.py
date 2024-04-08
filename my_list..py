340b98227fedd38a4fc4d18a30cd9a7df0e7f0ac
Tiffany <122724125+muchoritiffany@users.noreply.github.com>
2024-04-08 14:17:15 +0300

    Add files via upload

---
+++ my_list..py
@@ -0,0 +1,6 @@
+Python 3.11.9 (tags/v3.11.9:de54cf5, Apr  2 2024, 10:12:12) [MSC v.1938 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+=================== RESTART: C:/Windows/System32/my_list..py ===================
+Index of value 30 in my_list: 3
+Modified my_list: [10, 15, 20, 30, 40, 50, 60]
